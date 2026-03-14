abcf99b897d0e3a1f5bff4da1a83343c40a435c0
Abhishek Jaiswar <devabhishekjaiswar@gmail.com>
2026-03-14 18:23:05 +0530

    Add configs for deployment in production

--- backend/src/lib/env.js
+++ backend/src/lib/env.js
@@ -5,4 +5,5 @@ dotenv.config();
 export const ENV = {
   PORT: process.env.PORT,
   DB_URL: process.env.DB_URL,
+  ENVIRONMENT: process.env.ENVIRONMENT,
 };
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,22 @@
+import path from "path";
 import express from "express";
 import { ENV } from "./lib/env.js";
 
 const app = express();
 
+const __dirname = path.resolve();
+
 app.get("/", (req, res) => {
   res.status(200).json({ msg: "Success from Backend API" });
 });
+
+// Used in production when deployed
+if (ENV.ENVIRONMENT === "prod") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("/{*any}", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 app.listen(ENV.PORT, () => console.log("Server is running on port:", ENV.PORT));
