8f90bc3895ed23805fe98650416384a951200cd5
Abhishek Jaiswar <devabhishekjaiswar@gmail.com>
2026-03-21 23:50:05 +0530

    Move /api and /api/inngest endpoint to app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,8 @@
 import cors from "cors";
 import express from "express";
+import { serve } from "inngest/express";
 import path from "path";
+import { inngest, functions } from "./config/inngest.js";
 import { ENV } from "./lib/env.js";
 
 export const app = express();
@@ -9,6 +11,12 @@ export const app = express();
 app.use(express.json());
 // Credentials: true meaning?? => server allows a browser to include cookies on request
 app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
+
+app.get("/api", (_, res) => {
+  res.status(200).json({ msg: "Success from Backend API" });
+});
+
+app.get("/api/inngest", serve({ client: inngest, functions }));
 
 const __dirname = path.resolve();
 
