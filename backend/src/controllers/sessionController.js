f2c1eb2550a6d207b746f71f678724283d13f70e
Abhishek Jaiswar <devabhishekjaiswar@gmail.com>
2026-04-02 23:15:22 +0530

    fix(bug): add await before SessionDB modal call

--- backend/src/controllers/sessionController.js
+++ backend/src/controllers/sessionController.js
@@ -139,7 +139,7 @@ export async function endSession(req, res) {
     const { id } = req.params;
     const userId = req.user._id;
 
-    const session = Session.findById(id);
+    const session = await Session.findById(id);
 
     if (!session) return res.status(404).json({ message: "Session not found" });
 
