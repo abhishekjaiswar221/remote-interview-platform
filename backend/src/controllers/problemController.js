28aadce5f05429f228673cdfda188b1b6e711fdb
Abhishek Jaiswar <devabhishekjaiswar@gmail.com>
2026-04-15 19:21:01 +0530

    feat: add api endpoint /problem/update/:id to update problems

--- backend/src/controllers/problemController.js
+++ backend/src/controllers/problemController.js
@@ -15,13 +15,13 @@ export const addProblem = async (req, res) => {
       expectedOutput,
     } = req.body;
 
-    // Validation
+    // Validation for required fields
     const requiredFields = { id, title, difficulty, category };
     const missingFields = Object.entries(requiredFields)
       .filter(([_, value]) => !value)
       .map(([key]) => key);
 
-    if (!id || !title || !difficulty || !category) {
+    if (missingFields.length > 0) {
       return res.status(400).json({
         message: `Missing required ${missingFields.length > 1 ? "fields" : "field"}: ${missingFields.join(", ")}`,
       });
@@ -35,7 +35,7 @@ export const addProblem = async (req, res) => {
         .json({ message: "Problem with this ID already exists" });
     }
 
-    await Problem.create({
+    const newProblem = new Problem({
       id,
       title,
       difficulty,
@@ -47,12 +47,71 @@ export const addProblem = async (req, res) => {
       expectedOutput,
     });
 
+    await newProblem.save();
+
     return res.status(201).json({ message: "Problem added successfully!" });
   } catch (error) {
-    logger.error("Error in addProblem controller", {
+    logger.error("Error in addProblem controller", { stack: error.stack });
+    return res
+      .status(500)
+      .json({ message: "Internal Server Error", error: error.message });
+  }
+};
+
+export const updateProblem = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const {
+      title,
+      difficulty,
+      category,
+      description,
+      examples,
+      constraints,
+      starterCode,
+      expectedOutput,
+    } = req.body;
+
+    // Validation for required fields
+    const requiredFields = { id, title, difficulty, category };
+    const missingFields = Object.entries(requiredFields)
+      .filter(([_, value]) => !value)
+      .map(([key]) => key);
+
+    if (missingFields.length > 0) {
+      return res.status(400).json({
+        message: `Missing required ${missingFields.length > 1 ? "fields" : "field"}: ${missingFields.join(", ")}`,
+      });
+    }
+
+    const updateProblem = await Problem.findOneAndUpdate(
+      { id },
+      {
+        title,
+        difficulty,
+        category,
+        description,
+        examples,
+        constraints,
+        starterCode,
+        expectedOutput,
+      },
+      { new: true },
+    );
+
+    if (!updateProblem) {
+      return res.status(404).json({
+        message: `Problem with the ID ${id} not found`,
+      });
+    }
+
+    return res.status(200).json({ message: "Problem updated successfully!" });
+  } catch (error) {
+    logger.error("Error in updateProblem controller", {
       stack: error.stack,
     });
-    res
+
+    return res
       .status(500)
       .json({ message: "Internal Server Error", error: error.message });
   }
--- backend/src/routes/problemRoutes.js
+++ backend/src/routes/problemRoutes.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { addProblem } from "../controllers/problemController.js";
+import { addProblem, updateProblem } from "../controllers/problemController.js";
 
 const router = express.Router();
 
 router.post("/add", addProblem);
+router.put("/update/:id", updateProblem);
 
 export default router;
